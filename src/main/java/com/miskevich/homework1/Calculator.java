c258472b7846eeb6a9365a2b5e404ad6a1574f4a
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-07-22 09:51:39 +0300

    fix + add 3, 4 point

--- src/main/java/com/miskevich/homework1/Calculator.java
+++ src/main/java/com/miskevich/homework1/Calculator.java
@@ -5,78 +5,90 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
+        Scanner reader = new Scanner(System.in);
+
+        char operator;
+
         double num1;
 
         double num2;
 
         double result;
 
-        char operator;
-
-        Scanner reader = new Scanner(System.in);
-
-        while (true) {
-            try {
-                System.out.print("Enter the first number: ");
-                num1 = reader.nextDouble();
-                break;
-            } catch (Exception e) {
-                System.out.println("Error: Invalid input. Please try again.");
-                reader.nextLine();
-            }
-        }
-        while (true) {
-            try {
-                System.out.print("Enter the second number: ");
-                num2 = reader.nextDouble();
-
-                break;
-            } catch (Exception e) {
-                System.out.println("Error: Invalid input. Please try again.");
-                reader.nextLine();
-            }
-        }
-
-        while (true) {
-            try {
-                System.out.print("Choose an operator (+, -, *, /): ");
-                operator = reader.next().charAt(0);
-                if(operator == '+' || operator == '-' || operator == '*' || operator == '/')
+        do {
+            while (true) {
+                try {
+                    System.out.print("Enter the first number: ");
+                    num1 = reader.nextDouble();
                     break;
-                else
-                    throw new Exception();
-            } catch (Exception e) {
-                System.out.println("Error: Invalid operator. Please try again.");
-                reader.nextLine();
-            }
-        }
-
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error: Division by zero.");
-                    return;
+                } catch (Exception e) {
+                    System.out.println("Error: Invalid input. Please try again.");
+                    reader.nextLine();
                 }
-                break;
-            default:
-                System.out.println("Error: Invalid operator.");
-                return;
-        }
-        System.out.print("Result: ");
-        System.out.printf(num1 + " " + operator + " " + num2 + " = " + result);
+            }
+            while (true) {
+                try {
+                    System.out.print("Enter the second number: ");
+                    num2 = reader.nextDouble();
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Error: Invalid input. Please try again.");
+                    reader.nextLine();
+                }
+            }
+            while (true) {
+                try {
+                    System.out.print("Choose an operator (+, -, *, /): ");
+                    String input = reader.next();
+                    if (input.length() == 1) {
+                        operator = input.charAt(0);
+                        if (operator == '+' || operator == '-' || operator == '*' || operator == '/')
+                            break;
+                    }
+                    throw new Exception();
+                } catch (Exception e) {
+                    System.out.println("Error: Invalid operator. Please try again.");
+                    reader.nextLine();
+                }
+            }
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Error: Division by zero");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Error: Invalid operator.");
+                    return;
+            }
+
+            System.out.print("Result: ");
+            System.out.printf(num1 + " " + operator + " " + num2 + " = " + result);
+
+            while (true) {
+                System.out.println("\nDo you want to continue? (y/n): ");
+                String choice = reader.next();
+                if (choice.length() == 1) {
+                    if (choice.equalsIgnoreCase("y")) {
+                        break;
+                    } else if (choice.equalsIgnoreCase("n")) {
+                        System.exit(0);
+                    }
+                }
+                System.out.println("Error: Invalid input. Type 'y' to continue or 'n' to end: ");
+            }
+        } while (true);
     }
-
-
 }

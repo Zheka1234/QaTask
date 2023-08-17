f9eefb526c2c2b6f343fc3f957a2c91d21158e73
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-08-17 18:18:52 +0300

    Homework 2 add long int float

--- src/main/java/com/miskevich/homework1/service/NumberInput.java
+++ src/main/java/com/miskevich/homework1/service/NumberInput.java
@@ -5,4 +5,14 @@ import java.util.Scanner;
 public interface NumberInput {
 
     double getNumberInput(Scanner reader, String message);
+
+    float getNumberInputFloat(Scanner reader, String message);
+
+    long getNumberInputLong(Scanner reader, String message);
+
+    int getNumberInputInt(Scanner reader, String message);
+
+//    String getStringInput(Scanner reader, String message);
+
+
 }
--- src/main/java/com/miskevich/homework1/service/impl/NumberInputImpl.java
+++ src/main/java/com/miskevich/homework1/service/impl/NumberInputImpl.java
@@ -22,6 +22,79 @@ public class NumberInputImpl implements NumberInput {
             }
         }
         return number;
-    };
+    }
+
+    @Override
+    public float getNumberInputFloat(Scanner reader, String message) {
+        float number;
+
+        while (true) {
+            try {
+                System.out.print(message);
+                number = reader.nextFloat();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Invalid input. Please try again.");
+                reader.nextLine();
+            }
+        }
+        return number;
+    }
+
+    @Override
+    public long getNumberInputLong(Scanner reader, String message) {
+        long number;
+
+        while (true) {
+            try {
+                System.out.print(message);
+                number = reader.nextLong();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Invalid input. Please try again.");
+                reader.nextLine();
+            }
+        }
+        return number;
+    }
+
+    @Override
+    public int getNumberInputInt(Scanner reader, String message) {
+        int number;
+
+        while (true) {
+            try {
+                System.out.print(message);
+                number = reader.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Invalid input. Please try again.");
+                reader.nextLine();
+            }
+        }
+        return number;
+    }
+
+//        @Override
+//    public String getStringInput(Scanner reader, String message) {
+//        String number;
+//
+//        while (true) {
+//            try {
+//                System.out.print(message);
+//                number = String.valueOf(reader.nextInt());
+//                break;
+//            } catch (InputMismatchException e) {
+//                System.out.println("Error: Invalid input. Please try again.");
+//                reader.nextLine();
+//            }
+//        }
+//        return number;
+//    }
+//    @Override
+//    public String getStringInput(Scanner reader, String message) {
+//        System.out.print(message);
+//        return reader.nextLine();
+//    }
 
 }

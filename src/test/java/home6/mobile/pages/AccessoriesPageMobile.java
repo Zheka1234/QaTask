b8659e8535a845c643b47e525fcb9c156cf678d6
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-09-16 16:53:43 +0300

    fix 3 case

--- src/test/java/home6/mobile/pages/AccessoriesPageMobile.java
+++ src/test/java/home6/mobile/pages/AccessoriesPageMobile.java
@@ -2,7 +2,6 @@ package home6.mobile.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.ex.ElementNotFound;
 
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -11,143 +10,40 @@ public class AccessoriesPageMobile extends BasePageMobile {
 
     private String AMOUNT_AND_PRICE = "[\\d]{0,}[\\,]?[\\d]+[\\s][товар[а]?[ов]?\nот\\s[\\d]+[\\,]?[\\d]{0,}[\\s]р\\.]";
 
-    private void clickAccessories() {
+
+    public void clickAccessories() {
         SelenideElement element =
                 $x("//div[@class='catalog-navigation-list__aside-title' and text()=' Комплектующие ']");
         element.shouldBe(Condition.visible).click();
     }
 
-    public void checkAccessoriesPartFirst() {
-        log.info("checkAccessoriesPartFirst start");
-        clickAccessories();
-        try {
-            SelenideElement videoCard = $x("//a[@href='https://catalog.onliner.by/videocard']/span/span[2]");
-            SelenideElement videoCard1 = $x("//a[@href='https://catalog.onliner.by/videocard']/span/span[3]");
-            videoCard.shouldBe(Condition.visible);
-            videoCard1.should(Condition.matchText(AMOUNT_AND_PRICE));
+    private SelenideElement sectionThird(String sectionNameThird) {
+        SelenideElement element = $x("//div[.=' Комплектующие ']//..//a//span[@class='catalog-navigation-list__dropdown-title']" +
+                "[contains(text()," +
+                "'" + sectionNameThird + "')]");
 
-            SelenideElement cpu = $x("//a[@href='https://catalog.onliner.by/cpu']/span/span[2]");
-            SelenideElement cpu1 = $x("//a[@href='https://catalog.onliner.by/cpu']/span/span[3]");
-            cpu.shouldBe(Condition.visible);
-            cpu1.should(Condition.matchText(AMOUNT_AND_PRICE));
-
-            SelenideElement motherBoard = $x("//a[@href='https://catalog.onliner.by/motherboard']/span/span[2]");
-            SelenideElement motherBoard1 = $x("//a[@href='https://catalog.onliner.by/motherboard']/span/span[3]");
-            motherBoard.shouldBe(Condition.visible);
-            motherBoard1.should(Condition.matchText(AMOUNT_AND_PRICE));
-        } catch (ElementNotFound e) {
-            log.warn("Element not found: " + e.getMessage());
-
-        }
-        log.debug("checkAccessoriesPartFirst good");
-
+        return element;
     }
 
-    public void checkAccessoriesPartSecond() {
-        log.info("checkAccessoriesPartSecond start");
-        try {
-            SelenideElement ram = $x("//a[@href='https://catalog.onliner.by/dram']/span/span[2]");
-            SelenideElement ram1 = $x("//a[@href='https://catalog.onliner.by/dram']/span/span[3]");
-            ram.shouldBe(Condition.visible);
-            ram1.should(Condition.matchText(AMOUNT_AND_PRICE));
-
-            SelenideElement ssd = $x("//a[@href='https://catalog.onliner.by/ssd']/span/span[2]");
-            SelenideElement ssd1 = $x("//a[@href='https://catalog.onliner.by/ssd']/span/span[3]");
-            ssd.shouldBe(Condition.visible);
-            ssd1.should(Condition.matchText(AMOUNT_AND_PRICE));
-
-            SelenideElement hdd = $x("//a[@href='https://catalog.onliner.by/hdd']/span/span[2]");
-            SelenideElement hdd1 = $x("//a[@href='https://catalog.onliner.by/hdd']/span/span[3]");
-            hdd.scrollIntoView(true);
-            hdd.shouldBe(Condition.visible);
-            hdd1.should(Condition.matchText(AMOUNT_AND_PRICE));
-        } catch (ElementNotFound e) {
-            log.warn("Element not found: " + e.getMessage());
-
-        }
-        log.debug("checkAccessoriesPartSecond good");
-
+    public SelenideElement sectionFour() {
+        SelenideElement element = $x("//div[.=' Комплектующие ']//..//a//span[@class='catalog-navigation-list__dropdown-description']")
+                .shouldBe(Condition.matchText(AMOUNT_AND_PRICE));
+        return element;
     }
 
-    public void checkAccessoriesPartThird() {
-        log.info("checkAccessoriesPartThird start");
-        try {
-            SelenideElement fan = $x("//a[@href='https://catalog.onliner.by/fan']/span/span[2]");
-            SelenideElement fan1 = $x("//a[@href='https://catalog.onliner.by/fan']/span/span[3]");
-            fan.shouldBe(Condition.visible);
-            fan1.should(Condition.matchText(AMOUNT_AND_PRICE));
+    public boolean checkSectionPresenceFour(String sectionNameThird) {
+        log.info("checkSectionPresenceTwo start");
 
-            SelenideElement powerSupply = $x("//a[@href='https://catalog.onliner.by/powersupply']/span/span[2]");
-            SelenideElement powerSupply1 = $x("//a[@href='https://catalog.onliner.by/powersupply']/span/span[3]");
-            powerSupply.shouldBe(Condition.visible);
-            powerSupply1.should(Condition.matchText(AMOUNT_AND_PRICE));
+        sectionFour().shouldBe(Condition.visible);
 
-            SelenideElement frame = $x("//a[@href='https://catalog.onliner.by/chassis']/span/span[2]");
-            SelenideElement frame1 = $x("//a[@href='https://catalog.onliner.by/chassis']/span/span[3]");
-            frame.shouldBe(Condition.visible);
-            frame1.should(Condition.matchText(AMOUNT_AND_PRICE));
-        } catch (ElementNotFound e) {
-            log.warn("Element not found: " + e.getMessage());
-
-        }
-        log.debug("checkAccessoriesPartThird good");
-
+        return true;
     }
 
-    public void checkAccessoriesPartFourth() {
-        log.info("checkAccessoriesPartFourth start");
-        try {
-            SelenideElement accessory = $x("//a[@href='https://catalog.onliner.by/moddingpc']/span/span[2]");
-            SelenideElement accessory1 = $x("//a[@href='https://catalog.onliner.by/moddingpc']/span/span[3]");
-            accessory.shouldBe(Condition.visible);
-            accessory1.should(Condition.matchText(AMOUNT_AND_PRICE));
+    public boolean checkSectionPresenceThird(String sectionNameThird) {
+        log.info("checkSectionPresenceTwo start");
 
-            SelenideElement soundCard =
-                    $x("//a[@href='https://catalog.onliner.by/" +
-                            "soundcard?type_soundcard%5B0%5D=internal&type_soundcard%5Boperation%5D=union']/span/span[2]");
-            SelenideElement soundCard1 =
-                    $x("//a[@href='https://catalog.onliner.by/" +
-                            "soundcard?type_soundcard%5B0%5D=internal&type_soundcard%5Boperation%5D=union']/span/span[3]");
-            soundCard.shouldBe(Condition.visible);
-            soundCard1.should(Condition.matchText(AMOUNT_AND_PRICE));
+        sectionThird(sectionNameThird).shouldBe(Condition.visible);
 
-            SelenideElement network = $x("//a[@href='https://catalog.onliner.by/networkadapter']/span/span[2]");
-            SelenideElement network1 = $x("//a[@href='https://catalog.onliner.by/networkadapter']/span/span[3]");
-            network.shouldBe(Condition.visible);
-            network1.should(Condition.matchText(AMOUNT_AND_PRICE));
-        } catch (ElementNotFound e) {
-            log.warn("Element not found: " + e.getMessage());
-
-        }
-        log.debug("checkAccessoriesPartFourth good");
-
-    }
-
-    public void checkAccessoriesPartFifth() {
-        log.info("checkAccessoriesPartFifth start");
-        try {
-            SelenideElement optical = $x("//a[@href='https://catalog.onliner.by/optical']/span/span[2]");
-            SelenideElement optical1 = $x("//a[@href='https://catalog.onliner.by/optical']/span/span[3]");
-            optical.shouldBe(Condition.visible);
-            optical1.should(Condition.matchText(AMOUNT_AND_PRICE));
-
-            SelenideElement tvTuner =
-                    $x("//a[@href='https://catalog.onliner.by/" +
-                            "tvtuner?execution_tvtune%5B0%5D=in&execution_tvtune%5Boperation%5D=union']/span/span[2]");
-            SelenideElement tvTuner1 =
-                    $x("//a[@href='https://catalog.onliner.by/" +
-                            "tvtuner?execution_tvtune%5B0%5D=in&execution_tvtune%5Boperation%5D=union']/span/span[3]");
-            tvTuner.shouldBe(Condition.visible);
-            tvTuner1.should(Condition.matchText(AMOUNT_AND_PRICE));
-
-            SelenideElement mining = $x("//a[@href='https://catalog.onliner.by/mining_acs']/span/span[2]");
-            SelenideElement mining1 = $x("//a[@href='https://catalog.onliner.by/mining_acs']/span/span[3]");
-            mining.shouldBe(Condition.visible);
-            mining1.should(Condition.matchText(AMOUNT_AND_PRICE));
-        } catch (ElementNotFound e) {
-            log.warn("Element not found: " + e.getMessage());
-
-        }
-        log.debug("checkAccessoriesPartFifth good");
+        return true;
     }
 }
--- src/test/java/home6/mobile/tests/TestAccessoriesIphone.java
+++ src/test/java/home6/mobile/tests/TestAccessoriesIphone.java
@@ -2,12 +2,13 @@ package home6.mobile.tests;
 
 import home6.mobile.pages.AccessoriesPageMobile;
 import home6.mobile.pages.CatalogPageMobile;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class TestAccessoriesIphone {
 
     static {
-        System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
         System.setProperty("chromeoptions.mobileEmulation", "deviceName=iPhone 12 Pro");
 
     }
@@ -16,15 +17,73 @@ public class TestAccessoriesIphone {
 
     private AccessoriesPageMobile accessoriesPageMobile = new AccessoriesPageMobile();
 
-    @Test
-    public void testAccessories() {
+    @DataProvider(name = "sectionNames")
+    public Object[][] sectionNames() {
+        return new Object[][]{
+                {"Видеокарты"},
+                {"Процессоры"},
+                {"Материнские платы"},
+                {"Оперативная память"},
+                {"Системы охлождения"},
+                {"SSD"},
+                {"Жесткие диски"},
+                {"Корпуса"},
+                {"Блоки питания"},
+                {"Моддинг, аксессуары для системных блоков"},
+                {"Звуковые карты"},
+                {"Сетевые адаптеры"},
+                {"Оптические приводы"},
+                {"ТВ-тюнеры и карты видеозахвата "},
+                {"Аксессуары для майнинга"},
+        };
+    }
+
+    @Test(dataProvider = "sectionNames")
+    public void testCatalogSections(String sectionName) {
         catalogPage.openCatalogPage();
         catalogPage.openComputerAndNet();
-        accessoriesPageMobile.checkAccessoriesPartFirst();
-        accessoriesPageMobile.checkAccessoriesPartSecond();
-        accessoriesPageMobile.checkAccessoriesPartThird();
-        accessoriesPageMobile.checkAccessoriesPartFourth();
-        accessoriesPageMobile.checkAccessoriesPartFifth();
+        accessoriesPageMobile.clickAccessories();
+        SoftAssert soft = new SoftAssert();
+        soft.assertTrue(accessoriesPageMobile.checkSectionPresenceThird(sectionName));
+
+        soft.assertAll();
+
+    }
+
+    @DataProvider(name = "sectionNamesSecond")
+    public Object[][] sectionNamesSecond() {
+        return new Object[][]{
+                {"Видеокарты"},
+                {"Процессоры"},
+                {"Материнские платы"},
+                {"Оперативная память"},
+                {"Системы охлождения"},
+                {"SSD"},
+                {"Жесткие диски"},
+                {"Корпуса"},
+                {"Блоки питания"},
+                {"Моддинг, аксессуары для системных блоков"},
+                {"Звуковые карты"},
+                {"Сетевые адаптеры"},
+                {"Оптические приводы"},
+                {"ТВ-тюнеры и карты видеозахвата "},
+                {"Аксессуары для майнинга"},
+        };
+    }
+
+    @Test(dataProvider = "sectionNamesSecond")
+    public void testCatalogPrice(String sectionName) {
+        catalogPage.openCatalogPage();
+        catalogPage.openComputerAndNet();
+        accessoriesPageMobile.clickAccessories();
+        SoftAssert soft = new SoftAssert();
+        soft.assertTrue(accessoriesPageMobile.checkSectionPresenceFour(sectionName));
+        System.out.println(sectionName);
+        System.out.println();
+        soft.assertAll();
+
 
     }
 }
+
+

58f9596da9f66f79c232a75bda8bc0c034d910b1
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-09-18 15:11:26 +0300

    add root

--- src/test/java/home7/pages/GridPage.java
+++ src/test/java/home7/pages/GridPage.java
@@ -11,6 +11,9 @@ public class GridPage extends AuthorizationPage {
     @FindBy(xpath = "//div[@row-index='3']//div[@class='ag-cell ag-cell-not-inline-editing ag-cell-with-height ag-cell-value']")
     private WebElement cellClick;
 
+    @FindBy(xpath = "//div[@class='flex-1 post-trade__grid post-trade__grid_fullscreen']")
+    private WebElement root;
+
     @FindBy(xpath = "//span[@class='chart-settings__title chart-mobile-title']")
     private WebElement linesClick;
 

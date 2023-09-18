d2d35e4f0f6f2b4e0d63410aa4283d9c9dfa43a7
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-09-18 13:14:19 +0300

    fix

--- src/test/java/home7/pages/ScatterplotPage.java
+++ src/test/java/home7/pages/ScatterplotPage.java
@@ -1,6 +1,20 @@
 package home7.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class ScatterplotPage extends AuthorizationPage {
 
+    @FindBy(xpath = "//div[@class='app-title'][contains(text(),'Scatter-plot)]")
+    private WebElement openScatter;
+
+    @FindBy(xpath = "//div[@row-index='3']//div[@class='ag-cell ag-cell-not-inline-editing ag-cell-with-height ag-cell-value']")
+    private WebElement cellClick;
+
+    @FindBy(xpath = "//div[@class='scatter-plot-container']")
+    private WebElement scatterPlotContainer;
+
+    @FindBy(xpath = "//input[@class='autocomplete-input ng-untouched ng-pristine ng-valid']")
+    private WebElement scatterPlotInputs;
 
 }

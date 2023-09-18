40d4e29b3048a46b6be6df7c6b1cb28ddf6f2829
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-09-18 13:27:06 +0300

    fix

--- src/test/java/home7/pages/ScatterplotPage.java
+++ src/test/java/home7/pages/ScatterplotPage.java
@@ -8,13 +8,19 @@ public class ScatterplotPage extends AuthorizationPage {
     @FindBy(xpath = "//div[@class='app-title'][contains(text(),'Scatter-plot)]")
     private WebElement openScatter;
 
-    @FindBy(xpath = "//div[@row-index='3']//div[@class='ag-cell ag-cell-not-inline-editing ag-cell-with-height ag-cell-value']")
-    private WebElement cellClick;
+    @FindBy(xpath = "//div[text()='X Attribute']/following-sibling::*")
+    private WebElement xAttribute;
 
-    @FindBy(xpath = "//div[@class='scatter-plot-container']")
-    private WebElement scatterPlotContainer;
+    @FindBy(xpath = "//div[text()='Y Attribute']/following-sibling::*")
+    private WebElement yAttribute;
 
-    @FindBy(xpath = "//input[@class='autocomplete-input ng-untouched ng-pristine ng-valid']")
-    private WebElement scatterPlotInputs;
+    @FindBy(xpath = "//*[@name='intervalCountAutocomplete']//input")
+    private WebElement interval;
+
+    @FindBy(xpath = "//*[@class='scatter-plot-x-label']")
+    private WebElement xLabel;
+
+    @FindBy(xpath = "//*[@class='scatter-plot-y-label']")
+    private WebElement yLabel;
 
 }
